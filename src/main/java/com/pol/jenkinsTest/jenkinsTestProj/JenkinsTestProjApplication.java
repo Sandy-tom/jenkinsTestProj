3788cc8e1bbba291cfdbb0e0cc3a2591b3f93c80
Sandy-tom <santhoshkumartom@gmail.com>
2024-02-11 16:38:01 +0530

    first jenkins pipline commit

---
+++ src/main/java/com/pol/jenkinsTest/jenkinsTestProj/JenkinsTestProjApplication.java
@@ -0,0 +1,13 @@
+package com.pol.jenkinsTest.jenkinsTestProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pol/jenkinsTest/jenkinsTestProj/JenkinsTestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pol.jenkinsTest.jenkinsTestProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
